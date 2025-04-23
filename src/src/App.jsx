d51bd59705593bcc8552d0b86194ef79ead482f7
cyprus063 <lscinvestment01@gmail.com>
2025-04-23 14:28:30 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Hamba!</h1>
+      <p>Your go-to transport booking app for daily commuters.</p>
+    </div>
+  )
+}
+
+export default App
